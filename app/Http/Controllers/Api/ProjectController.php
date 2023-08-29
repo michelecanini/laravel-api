dd4fa355334ab827479b554b8f3d7485c7e35eb1
michelecanini <studio.michelecanini@gmail.com>
2023-08-29 17:25:31 +0200

    [NEW] added function index in ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json([
+
+            'success' => true,
+            'result' => $projects
+
+        ]);
+    }
 }
