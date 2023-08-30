a0b08ebc3c9c9d0425a067c51977804ad4ea5c74
michelecanini <studio.michelecanini@gmail.com>
2023-08-31 00:53:00 +0200

    [NEW] inserted paginate method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,7 +12,8 @@ class ProjectController extends Controller
     public function index()
     {
         //$projects = Project::all();
-        $projects = Project::with('type', 'technologies')->get();
+        //$projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
         return response()->json([
 
             'success' => true,
