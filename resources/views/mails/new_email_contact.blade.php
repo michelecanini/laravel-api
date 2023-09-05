551891e255e3b793fbe6642f7a192dc7c4971cea
michelecanini <studio.michelecanini@gmail.com>
2023-09-05 23:46:32 +0200

    [NEW] added migration and text email

---
+++ resources/views/mails/new_email_contact.blade.php
@@ -0,0 +1,8 @@
+<h1>Nuovo contatto ricevuto da Boolfolio</h1>
+<p>
+    Hai ricevuto un nuovo messaggio:
+    Utente: {{ $lead->name }}
+    Email: {{ $lead->email }}
+    Contenuto: <br/>
+    {{ $lead->message }}
+</p>
