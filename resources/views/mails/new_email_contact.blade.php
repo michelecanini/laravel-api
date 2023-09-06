d52c93f0005c5baddc52df4d85f07b9bc59484e3
michelecanini <studio.michelecanini@gmail.com>
2023-09-06 15:23:39 +0200

    [TWEAK] adj style message mailtrap

--- resources/views/mails/new_email_contact.blade.php
+++ resources/views/mails/new_email_contact.blade.php
@@ -1,8 +1,9 @@
 <h1>Nuovo contatto ricevuto da Boolfolio</h1>
 <p>
-    Hai ricevuto un nuovo messaggio:
-    Utente: {{ $lead->name }}
-    Email: {{ $lead->email }}
-    Contenuto: <br/>
-    {{ $lead->message }}
+    Hai ricevuto un nuovo messaggio:<br><br>
+    Utente: <strong>{{ $lead->name }}</strong><br><br>
+    Email: <strong>{{ $lead->email }}<strong><br><br>
+    Messaggio:<br><br>
+    <strong>{{ $lead->message }}<strong>
 </p>
+
